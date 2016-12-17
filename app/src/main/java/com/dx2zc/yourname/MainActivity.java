965c9abe409235ac469aae26395923b9c49d4f81
dx2zc <dongxun2zc1314@gmail.com>
2016-12-17 23:13:37 +0800

    Initial commit

---
+++ app/src/main/java/com/dx2zc/yourname/MainActivity.java
@@ -0,0 +1,74 @@
+package com.dx2zc.yourname;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import info.hellovass.snowingview.widgets.SnowingView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SnowingView snowingView;
+    private MediaPlayer music;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        snowingView = (SnowingView) findViewById(R.id.snowing_view);
+        //开始下雪
+        snowingView.startFall();
+
+        music = new MediaPlayer();
+        music = MediaPlayer.create(this,R.raw.qianqianshishi);
+        try {
+            if (music != null){
+                music.stop();
+            }
+            music.prepare();
+            music.start();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(1,1,0,"关于");
+        menu.add(1,2,1,"退出");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case 0:
+                //TODO
+                break;
+            case 1:
+                finish();break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //停止下雪
+        snowingView.stopFall();
+        //退出时关闭音乐
+        try {
+            if (music != null){
+                music.stop();
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ snowingview/src/test/java/info/hellovass/snowingview/ExampleUnitTest.java
@@ -0,0 +1,14 @@
+package info.hellovass.snowingview;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+  @Test public void addition_isCorrect() throws Exception {
+    assertEquals(4, 2 + 2);
+  }
+}
